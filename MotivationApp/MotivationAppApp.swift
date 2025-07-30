888da5408700aee4ffb53b871326811221134c59
Ivan Semikin <i.semikin.94@icloud.com>
2025-07-30 20:20:13 +0200

    Initial Commit

---
+++ MotivationApp/MotivationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationAppApp.swift
+//  MotivationApp
+//
+//  Created by Иван Семикин on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MotivationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
